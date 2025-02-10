80fb4368cda845453a95c43dd6ab5e4d347887e3
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2025-02-10 12:03:39 +0200

    npm init + basic express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+    console.log(`Client with ip "${req.ip}" connected`);
+    res.send("<h1>tahash moment</h1>");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
