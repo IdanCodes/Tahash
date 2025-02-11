db91682d628749c35a94c2c9e5054799492271da
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2025-02-11 16:35:23 +0200

    Moved to WebStorm + .gitignore + index.js->server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 
 
+// home page
 app.get("/", (req, res) => {
     console.log(`Client with ip "${req.ip}" connected`);
     res.send("<h1>tahash moment</h1>");
